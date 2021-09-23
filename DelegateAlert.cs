f8197ef3156b14020c6ffd958a8d33b7f1894991
XanderChua <72645612+XanderChua@users.noreply.github.com>
2021-09-23 18:44:11 +0800

    Add files via upload

---
+++ DelegateAlert.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace DelegateAlert
+{
+    public delegate void EventHandler(string place, double intensity, double probability);
+    class Tsunami //publisher
+    {
+        public event EventHandler send;
+        public string place;
+        public double intensity;
+        double probability;
+        public void GetInputTsunami(string place, double intensity)
+        {
+            this.place = place;
+            this.intensity = intensity; 
+            CalculateTsunamiProbability();
+            Notify();
+        }
+        public void CalculateTsunamiProbability()
+        {
+            probability = intensity * 0.7 + 0.3;
+        }
+        private void Notify()
+        {
+            if(send != null)
+            {
+                send(place, intensity, probability);
+            }
+        }
+    }
+    class Earthquake //subscriber
+    {
+        private Tsunami tsunami;
+        public void SubscribeTsunami(Tsunami tsunami)
+        {
+            this.tsunami = tsunami;
+            tsunami.send += Tsunami_send;
+        }
+        public void Tsunami_send(string place, double intensity, double probability)
+        {
+            Console.WriteLine("The earthquake intensity of " + place + " is Magnitude: " + intensity + ".");
+            Console.WriteLine("The probability of " + place + " having a tsunami is: " + probability + "%.");
+        }
+    }
+    class DelegateAlert
+    {
+        static void Main(string[] args)
+        {
+            Earthquake earthquake = new Earthquake();
+            Tsunami tsunami = new Tsunami();
+
+            Console.WriteLine("--Delegate Earthquake Tsunami Alert--");      
+            Console.WriteLine("Enter location of earthquake:");
+            string place = Console.ReadLine();
+            Console.WriteLine("Enter intensity of earthquake:");
+            double intensity = double.Parse(Console.ReadLine());
+
+            earthquake.SubscribeTsunami(tsunami);
+            tsunami.GetInputTsunami(place, intensity);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DelegateCounterPrintEvent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace DelegateCounterPrintEvent
+{
+    public delegate void EventHandler(int count);   
+    class Counter
+    {
+        public event EventHandler send;
+        int count;
+        public void GetInput(int count)
+        {
+            this.count = count;
+            Notify();
+            checkCounter();
+        }
+        public void checkCounter()
+        {
+            if (count == 20)
+            {
+                Console.WriteLine("Event Received");
+            }
+        }
+        private void Notify()
+        {
+            if (send != null)
+            {
+                send(count);
+            }
+        }
+    }
+    class DelegateCounterPrintEvent
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Delegate Counter--");
+            Counter counter = new Counter();
+            counter.send += Counter_send;         
+            for (int i = 1; i < 21; i++)
+            {
+
+                counter.GetInput(i);         
+            }
+           
+            Console.ReadLine();
+        }
+        private static void Counter_send(int count)
+        {
+            Console.WriteLine("Counter: " + count);
+            Thread.Sleep(500);
+        }
+    }
+}
